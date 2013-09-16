1d50786c48450527b682a92a491952d614e189e2
Arabella Martelli <amartell@lxplus405.cern.ch>
2013-09-16 18:17:45 +0200

    first commit

---
+++ lancia.sh
@@ -0,0 +1,30 @@
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_Data.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_DYJetsToLL.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_TTGJets.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_TTJets.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_WGToLNuG.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_ZZJetsTo2L2Q.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_Zgg_dR02.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_dipho_Box_25.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_dipho_Box_250.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_diphojet.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_ggh_m125_8TeV.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_gjet_20_pf.cfg           Medium
+#./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_gjet_20_ff.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_gjet_40_pf.cfg           Medium
+#./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_gjet_40_ff.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_qcd_30_ff.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_qcd_30_pf.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_qcd_40_ff.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_qcd_40_pf.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_ttgg_dR02.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_tth_m125_8TeV.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_vbf_m125_8TeV.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_MCBackgrounds_wzh_m125_8TeV.cfg           Medium
+./bin/FillHistograms_Trees.exe cfg/FillHistograms_Trees_Radion_nm_m300_.cfg           Medium
+
+
+
+
+
+
